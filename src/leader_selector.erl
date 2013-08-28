df183f160ed0a14dcf3363b89049f9b8ff1d2f49
Lol4t0 <sidorov.ij@gmail.com>
2013-08-29 02:00:07 +0400

    simplifying, added worker implementation.

--- src/leader_selector.erl
+++ src/leader_selector.erl
@@ -1,80 +1,77 @@
 -module(leader_selector).
 -behaviour(gen_server).
 
--export([start/5, start_link/5, init/1, terminate/2, handle_call/3, handle_cast/2, handle_info/2, code_change/3]).
+-export([start/6, start_link/6, stop/1, init/1, terminate/2, handle_call/3, handle_cast/2, handle_info/2, code_change/3]).
 
 -export([behaviour_info/1]).
 behaviour_info(callbacks) ->
-    [{init,1}, {restart_leader,1}, {restart_idle,2}, {address,1}, {terminate,1}].
+    [{restart_leader,1}, {restart_idle,2}].
 
 
-log(Msg, Args) when is_list(Args) ->
-	io:format(lsts:concat("MASTERED_TEST::", Msg, "~n"), Args);
-log(msg, arg) ->
-	log(msg, [arg]).
+log(Msg, Args) ->
+	io:format("MASTERED_TEST::" ++ Msg ++ "~n", Args).
 
 %% The startfunction.
-start(ConnectionPId, WorkerModule, WorkerParams, Prefix, NodeNum) ->
-    gen_server:start(?MODULE, [ConnectionPId, WorkerModule, WorkerParams, Prefix, NodeNum], []).
+start(ConnectionPId, WorkerModule, Address, Prefix, NodeNum, WorkerRef) ->
+    gen_server:start(?MODULE, [ConnectionPId, WorkerModule, Address, Prefix, NodeNum, WorkerRef], []).
 
-start_link(ConnectionPId, WorkerModule, WorkerParams, Prefix, NodeNum) ->
-    gen_server:start_link(?MODULE, [ConnectionPId, WorkerModule, WorkerParams, Prefix, NodeNum], []).
+start_link(ConnectionPId, WorkerModule, Address, Prefix, NodeNum, WorkerRef) ->
+    gen_server:start_link(?MODULE, [ConnectionPId, WorkerModule, Address, Prefix, NodeNum, WorkerRef], []).
 
 
-init([ConnectionPId, WorkerModule, WorkerParams, Prefix, NodeNum]) ->
+init([ConnectionPId, WorkerModule, Address, Prefix, NodeNum, WorkerRef]) ->
 	SelfNodeName = io_lib:format("~s/~w", [Prefix, NodeNum]),
-	log("Prefix: ~s", Prefix),
-	{ok, _Path} = ezk:create(ConnectionPId, SelfNodeName, WorkerModule:address(WorkerParams), e), % e for EPHEMERAL
-	WorkerState = WorkerModule:init(WorkerParams),
-	reelect({_SelfState = {ConnectionPId, WorkerModule, Prefix, NodeNum}, WorkerState}).
+	log("Prefix: ~s", [Prefix]),
+	{ok, _Path} = ezk:create(ConnectionPId, SelfNodeName, Address, e), % e for EPHEMERAL
+	reelect({ConnectionPId, WorkerModule, Prefix, NodeNum, WorkerRef}).
 
-reelect(FullState = {{ConnectionPId, _WorkerModule, Prefix, NodeNum}, _WorkerState}) ->
+reelect(State = {ConnectionPId, _WorkerModule, Prefix, NodeNum, _WorkerRef}) ->
 	{ok, List} = ezk:ls(ConnectionPId, Prefix, self(), nodes_changed),
 	ActiveNodes = lists:sort(lists:map (fun(<<Path>>) -> list_to_integer(Path) end, List)),
-	log("active nodes: ~w", ActiveNodes),
+	log("active nodes: ~w", [ActiveNodes]),
 	_Return = case lists:takeWhile(fun(N) -> N < NodeNum end) of
 		[] ->
 			log("starting as leader", []),
-			restart_as_leader(FullState);
+			restart_as_leader(State);
 		Nodes ->
 			ActiveNode = lists:last(Nodes),
-			log("starting as idle with active node: ~w", ActiveNode),
-			restart_as_idle(FullState, ActiveNode)
+			log("starting as idle with active node: ~w", [ActiveNode]),
+			restart_as_idle(State, ActiveNode)
 	end.
 
-restart_as_leader({SelfState = {_ConnectionPId, WorkerModule, _Prefix, _NodeNum}, WorkerState}) ->
-	NewWorkerState = WorkerModule:restart_leader(WorkerState),
-	{SelfState, NewWorkerState, leader}.
+restart_as_leader(SelfState = {_ConnectionPId, WorkerModule, _Prefix, _NodeNum, WorkerRef}) ->
+	WorkerModule:restart_leader(WorkerRef),
+	{SelfState, leader}.
 
-restart_as_idle({SelfState = {ConnectionPId, WorkerModule, Prefix, _NodeNum}, WorkerState}, ActiveNode) ->
+restart_as_idle(SelfState = {ConnectionPId, WorkerModule, Prefix, _NodeNum, WorkerRef}, ActiveNode) ->
 	ActiveNodePath = io_lib:format("~s/~w", [Prefix, ActiveNode]),
-	log("active node path: ~s", ActiveNodePath),
+	log("active node path: ~s", [ActiveNodePath]),
 	case ezk:get(ConnectionPId, ActiveNodePath, active_node_changed) of
 		{ok, {<<Address>>, _Stat}} ->
-			NewWorkerState = WorkerModule:restart_idle(Address, WorkerState),
-			{SelfState, NewWorkerState, idle};
+			WorkerModule:restart_idle(WorkerRef, Address),
+			{SelfState, idle};
 		{error, no_dir} -> % node deleted while electing
-			reelect({SelfState, WorkerState})
+			reelect(SelfState)
 	end.
 
 
-terminate(shutdown, {{ConnectionPId, WorkerModule, Prefix, NodeNum}, WorkerState, _Mode}) ->
+terminate(_Reason, {{ConnectionPId, _WorkerModule, Prefix, NodeNum, _WorkerRef}, _Mode}) ->
 	NodePath = io_lib:format("~s/~w", [Prefix, NodeNum]),
 	ezk:delete(ConnectionPId, NodePath),
-	WorkerModule:terminate(WorkerState),
     ok.
 
-handle_info({nodes_changed, {_Path, child_chnaged, _N}}, {SelfState, WorkerState, leader}) ->
-	reelect({SelfState, WorkerState});
-handle_info({active_node_changed, {_Path, _Action, _N}}, {SelfState, WorkerState, idle}) ->
-	reelect({SelfState, WorkerState});
+stop(Ref) ->
+	gen_server:call(Ref, stop).
+
+handle_info({nodes_changed, {_Path, child_chnaged, _N}}, {State, leader}) ->
+	reelect(State);
+handle_info({active_node_changed, {_Path, _Action, _N}}, {State, idle}) ->
+	reelect(State);
 handle_info(_, State) ->
 	State.
 
-
-
-handle_call(no_call_allowed, _From, _State) ->
-	error.
+handle_call(stop, _From, State) ->
+	{stop, normal, State}.
 
 handle_cast(no_cast_allowed, _State) ->
 	error.
--- src/worker.erl
+++ src/worker.erl
@@ -1,9 +1,67 @@
 -module(worker).
--export([worker_main/2]).
+-behaviour(gen_server).
+-behaviour(leader_selector).
+-export([start/3, start_link/3, stop/1,
+		 restart_leader/1, restart_idle/2,
+		 client_request/1,
+		 init/1, terminate/2, handle_call/3, handle_cast/2, handle_info/2, code_change/3]).
 
-worker_main(K, ServerId) ->
-	receive 
-		{Sender, Request} -> 
-			Sender ! {ok, Request, K, ServerId}
-	end,
-	worker_main(K, ServerId).
+-define(PREFIX, "/mastered_test/election/worker~w").
+
+start(ConnectionPid, NodeNum, {SelfAddress, WorkerNum}) ->
+	gen_server:start(?MODULE, [ConnectionPid, NodeNum, SelfAddress, WorkerNum], []).
+
+start_link(ConnectionPid, NodeNum, {SelfAddress, WorkerNum}) ->
+	gen_server:start_link(?MODULE, [ConnectionPid, NodeNum, SelfAddress, WorkerNum], []).
+
+stop(Ref) ->
+	gen_server:call(Ref, stop).
+
+
+
+init([ConnectionPid, NodeNum, SelfAddress, WorkerNum]) ->
+	WorkerZNodePath = io_lib:format(?PREFIX, [WorkerNum]),
+	SelectorRef = leader_selector:start(ConnectionPid, ?MODULE, SelfAddress, WorkerZNodePath, NodeNum, self()),
+	{SelfAddress, WorkerNum, SelectorRef, starting}.
+
+terminate(_Reason, {_SelfAddress, _WorkerNum, SelectorRef, _Mode}) ->
+	leader_selector:stop(SelectorRef).
+
+
+restart_leader(Ref) ->
+	gen_server:cast(Ref, leader).
+
+restart_idle(Ref, Address) ->
+	gen_server:cast(Ref, {idle, Address}).
+
+client_request(Ref) ->
+	gen_server:call(Ref, client_request).
+
+
+
+handle_call(client_request, _From, State ) ->
+	Answer = case State of
+		{_SelfAddress, _WorkerNum, _SelectorRef, starting} ->
+			{error, "Worker is not initialized yet."};
+		{SelfAddress, WorkerNum, _SelectorRef, leader} ->
+			{ok, io_lib:format("The Worker ~w on the node ~s is answering you, human. 42.~n", [WorkerNum, SelfAddress])};
+		{SelfAddress, WorkerNum, _SelectorRef, {idle, ActiveAddress}} ->
+			{ok, io_lib:format("Worker ~w on the node ~s is not in a position to answer you, the Human. Ask node ~s, please~n.",
+						   [WorkerNum, SelfAddress, ActiveAddress])}
+		end,
+	{reply, Answer, State};
+
+handle_call(stop, _From, State) ->
+	{stop, normal, State}.
+
+
+handle_cast(S, {SelfAddress, WorkerNum, SelectorRef, _Mode}) ->
+	NewState = {SelfAddress, WorkerNum, SelectorRef, S},
+	{noreply, NewState}.
+
+
+code_change(_, _, _) ->
+	error.
+
+handle_info(_,_) ->
+	error.
